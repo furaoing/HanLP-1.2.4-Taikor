c5db35ff53b76f35a7d3545f386b1096bf374815
furaoing <furaoing@hotmail.com>
2016-04-15 18:34:36 +0800

    Added Skeleton of pattern module

--- src/main/java/com/hankcs/hanlp/ie/pattern/PatternEngine.java
+++ src/main/java/com/hankcs/hanlp/ie/pattern/PatternEngine.java
@@ -4,6 +4,6 @@ package com.hankcs.hanlp.ie.pattern;
  * Created by roy on 2016/4/15.
  */
 
-public class Regex {
+public class PatternEngine {
 
 }
---
+++ src/main/java/com/hankcs/hanlp/ie/pattern/PatternExtractor.java
@@ -0,0 +1,12 @@
+package com.hankcs.hanlp.ie.pattern;
+
+/**
+ * Created by roy on 2016/4/15.
+ */
+
+public class PatternExtractor {
+    PatternEngine engine;
+
+
+}
+
